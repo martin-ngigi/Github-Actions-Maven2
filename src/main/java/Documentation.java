8753a3e9ef36d43288842f4cf137936e672cfdf5
martin-ngigi <martinwainaina001@gmail.com>
2022-10-22 12:51:36 +0300

    Hint to solving the error

--- src/main/java/Documentation.java
+++ src/main/java/Documentation.java
@@ -2,4 +2,13 @@ public class Documentation {
 //1    Youtube Link : https://www.youtube.com/watch?v=2XgE1tyFgvU&ab_channel=BrandanJones
 //2    Github Link: https://github.com/discospiff/GitHubActionsQuickStart
 //3. Use JDK 11 : https://www.techspot.com/downloads/5553-java-jdk.html
+    /**
+     * 4. Hint to solve the error
+     * Link-> https://stackoverflow.com/questions/13170860/failed-to-execute-goal-org-apache-maven-pluginsmaven-surefire-plugin2-10test
+     * 4.1 First delete the maven.yml in Github.
+     * 4.2 Then run the following intellij terminal
+     *              mvn clean install -U
+     *  4.3 Then create another maven.yml action in github.
+     *
+     *  **/
 }
