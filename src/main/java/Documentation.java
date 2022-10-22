83fd2607deb0f9f8513e27aeb31966f92a734317
martin-ngigi <martinwainaina001@gmail.com>
2022-10-22 13:50:13 +0300

    Updated documentation

--- src/main/java/Documentation.java
+++ src/main/java/Documentation.java
@@ -3,7 +3,12 @@ public class Documentation {
 //1    Youtube Link : https://www.youtube.com/watch?v=2XgE1tyFgvU&ab_channel=BrandanJones
 //2    Github Link: https://github.com/discospiff/GitHubActionsQuickStart
 //3. Use JDK 11 : https://www.techspot.com/downloads/5553-java-jdk.html
+
     /**
+     *      * 2. To add workflow, In github click Actions > then select Gradle, then create maven.yml
+     *      *    Then git pull
+     *
+     *
      * 4. Hint to solve the error
      * Link-> https://stackoverflow.com/questions/13170860/failed-to-execute-goal-org-apache-maven-pluginsmaven-surefire-plugin2-10test
      * 4.1 First delete the maven.yml in Github.
