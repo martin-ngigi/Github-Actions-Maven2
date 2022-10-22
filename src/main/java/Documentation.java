a2482d7faab7055cd07f68f070d6b1c80ece77ae
martin-ngigi <martinwainaina001@gmail.com>
2022-10-22 12:26:43 +0300

    Added Vehicle Class, Test and Maven

---
+++ src/main/java/Documentation.java
@@ -0,0 +1,5 @@
+public class Documentation {
+//1    Youtube Link : https://www.youtube.com/watch?v=2XgE1tyFgvU&ab_channel=BrandanJones
+//2    Github Link: https://github.com/discospiff/GitHubActionsQuickStart
+//3. Use JDK 11 : https://www.techspot.com/downloads/5553-java-jdk.html
+}
