94c93b03a92dbc2a56c6a6711102b53841f29193
martin-ngigi <martinwainaina001@gmail.com>
2022-10-22 13:12:13 +0300

    Calc Test should Fail

--- src/test/java/SimpleCalculatorTest.java
+++ src/test/java/SimpleCalculatorTest.java
@@ -6,7 +6,7 @@ class SimpleCalculatorTest {
     @Test
     public void calSumTest(){
         SimpleCalculator calculator = new SimpleCalculator();
-        int calc = calculator.calcSum(2,3);
+        int calc = calculator.calcSum(2,4);
         assertEquals(5,calc ); //This Test Should Pass since 2+3 equals to 3
     }
 }
