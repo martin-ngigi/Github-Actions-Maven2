b25c3335aca31f20b292c23a1e2cfea84a021284
martin-ngigi <martinwainaina001@gmail.com>
2022-10-22 13:24:28 +0300

    Added IDEA used

--- src/main/java/Documentation.java
+++ src/main/java/Documentation.java
@@ -1,4 +1,5 @@
 public class Documentation {
+    /**IDEA used is intellij Community**/
 //1    Youtube Link : https://www.youtube.com/watch?v=2XgE1tyFgvU&ab_channel=BrandanJones
 //2    Github Link: https://github.com/discospiff/GitHubActionsQuickStart
 //3. Use JDK 11 : https://www.techspot.com/downloads/5553-java-jdk.html
